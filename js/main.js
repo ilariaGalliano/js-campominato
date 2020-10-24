01bfa5154b59dfba352f99907b4d05eda16bf5b8
IlariaGalliano <ilaria.galliano7@gmail.com>
2020-10-24 13:47:55 +0200

    js-campominato
    Completo

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
 /*************************
 *****Campo Minato semplice
 *************************/
-// In seguito deve chiedere all'utente (100 - 16) volte
-// di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// L'utente non può inserire più volte lo stesso numero.
-// Se il numero è presente nella lista dei numeri generati (bombe), la partita termina,
-// altrimenti si continua chiedendo all'utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero "vietato"
-// raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio,
-// cioè il numero di volte che l'utente ha inserito un numero consentito.
+
 
 // BONUS: (da fare solo se funziona tutto il resto)
 // all'inizio il software richiede anche una difficoltà all'utente
@@ -18,8 +10,32 @@
 // con difficoltà 1 =>  tra 1 e 80
 // con difficoltà 2 => tra 1 e 50
 
+var level = parseInt( prompt('Scegli livello : \n 0 (Facile)\n 1 (Medio)\n 2 (Difficile)'));
+
+numberBombs = 16;
+numberMax = '';
+
+switch (level) {
+  case 0:
+    numberBombs = 16;
+    numberMax = 100;
+    level = 'Facile';
+    break;
+  case 1:
+    numberBombs = 16;
+    numberMax = 80;
+    level = 'Medio';
+    break;
+  case 2:
+    numberBombs = 16;
+    numberMax = 50;
+    level = 'Difficile';
+}
+
+
 // Il computer deve generare 16 numeri casuali (bombe) tra 1 e 100.
 var randNumber = [];
+var user = []; // per inserire dati utente
 var size = 16;
 
 // // Uso la funzione personalizzata
@@ -34,6 +50,44 @@ while (randNumber.length < size) {
   }
 }
 console.log(randNumber);
+
+// possibilità utente
+var userNumber = 84;
+
+
+// In seguito deve chiedere all'utente (100 - 16) volte
+// di inserire un numero alla volta, sempre compreso tra 1 e 100.
+
+for (var i = 0; i < userNumber; i++) {
+  var insertNumber = parseInt( prompt('inserisci un numero da 1 a 100'));
+
+  while (user.includes(insertNumber) || isNaN(insertNumber) ) {
+      insertNumber = parseInt( prompt('inserisci un altro numero'));
+  }
+  user.push(insertNumber);
+
+  if (! randNumber.includes(insertNumber)) { //verifico che non c'è un numero bomba qui dentro
+    console.log('numero valido, continua');
+  }
+  else {
+    console.log('numero non valido');
+    alert('Hai perso');
+    document.getElementById('text').innerHTML = 'Hai perso';
+    break;
+  }
+}
+
+if (userNumber == user.length) {
+  console.log('Hai vinto');
+  alert('Hai vinto');
+  document.getElementById('text').innerHTML = 'Hai vinto';
+}
+
+// Risultato finale:
+document.getElementById('level').innerHTML = 'Livello di difficoltà scelto: ' + level;
+document.getElementById('tentativi').innerHTML = 'Lista numeri inseriti: ' + user;
+document.getElementById('numeri-bomba').innerHTML = 'Numeri bomba: '+ randNumber;
+
 
 // Generatore numeri random con una funzione
 function  getRandom(min, max) {
